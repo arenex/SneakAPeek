ebfbca16848d0644e00139030e00c8dc5ae79697
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-03-17 10:53:17 -0400

    added basic express server

--- server.js
+++ server.js
@@ -79,3 +79,12 @@ function __setupWebSocket() {
         });
     });
 }
+
+const path = require("path");
+const express = require("express");
+const app = express();
+const dir = path.join(__dirname, "./");
+app.use(express.static(dir));
+app.listen(3000, function() {
+    console.log("Listening on http://localhost:3000/");
+});
