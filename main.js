069437818b704029e256d6fba55db7501adbeb04
arenex <starkvad@hotmail.com>
2018-03-18 18:53:11 -0400

    make configurable

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+process.title = 'run-server';
+
+var Server = require('./server.js');
+var config = require('./config.js');
+const server = Server(config);
+
+const opn = require('opn');
+opn(config.setBrowserUrl);
--- server.js
+++ server.js
@@ -5,55 +5,58 @@ const path = require("path");
 const express = require("express");
 const port = 3000;
 const app = express();
-
 var ffmpegBusy = false;
-var Driver = require('./driver.js');
-var driver = new Driver(
-    null, //twitchID TODO
-    __dirname,
-    null //ffmpegPath TODO
-);
 
-// routes
-const router = express.Router();
-router.get('/', (req, res) => {
-    res.json({
-        message: "hello world"
-    });
-});
-router.get('/streams', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    // parse streams from query parameter
-    const streams = JSON.parse(req.query.streams);
+function init({twitchID, ffmpegPath}) {
+    var Driver = require('./driver.js');
+    var driver = new Driver(
+        twitchID,
+        __dirname,
+        ffmpegPath
+    );
 
-    if (!ffmpegBusy && streams && streams.length) {
-        ffmpegBusy = true;
-        driver.run(streams).then(channelInfos => {
-            console.log(channelInfos);
-            ffmpegBusy = false;
-            for(let channel in channelInfos){
-                if (channelInfos[channel].imgUrl) {
-                    channelInfos[channel].imgUrl = `http://${path.join(`localhost:${port}`, channelInfos[channel].imgUrl)}`;
-                }
-            }
-            res.json({
-                streams: channelInfos
-            });
-        });
-    } else {
+    // routes
+    const router = express.Router();
+    router.get('/', (req, res) => {
         res.json({
-            message: "something went wrong"
+            message: "hello world"
         });
-    }
-});
+    });
+    router.get('/streams', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        // parse streams from query parameter
+        const streams = JSON.parse(req.query.streams);
 
-app.use(express.static(path.join(__dirname, "./"), {
-    etag: false,
-    maxage: '0h'
-}));
-app.use('/api', router);
+        if (!ffmpegBusy && streams && streams.length) {
+            ffmpegBusy = true;
+            driver.run(streams).then(channelInfos => {
+                console.log(channelInfos);
+                ffmpegBusy = false;
+                for(let channel in channelInfos){
+                    if (channelInfos[channel].imgUrl) {
+                        channelInfos[channel].imgUrl = `http://${path.join(`localhost:${port}`, channelInfos[channel].imgUrl)}`;
+                    }
+                }
+                res.json({
+                    streams: channelInfos
+                });
+            });
+        } else {
+            res.json({
+                message: "something went wrong"
+            });
+        }
+    });
 
-// start the server
-app.listen(port, function () {
-    console.log(`Listening on http://localhost:${port}/`);
-});
+    app.use(express.static(path.join(__dirname, "./"), {
+        etag: false,
+        maxage: '0h'
+    }));
+    app.use('/api', router);
+
+    // start the server
+    app.listen(port, function () {
+        console.log(`Listening on http://localhost:${port}/`);
+    });
+}
+module.exports = init;
